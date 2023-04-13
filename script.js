e69168be6d814863b60380170a01af7beddd96ac
seshlouu <86013506+seshlouu@users.noreply.github.com>
2023-04-13 20:18:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-et container = document.querySelector(".container");
+let container = document.querySelector(".container");
 let gridButton = document.getElementById("submit-grid");
 let clearGridButton = document.getElementById("clear-grid");
 let gridWidth = document.getElementById("width-range");
